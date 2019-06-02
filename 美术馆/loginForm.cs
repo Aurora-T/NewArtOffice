bfedf7022c15deb241206b78d17053c00a1784ab
18866013323 <18866013323@163.com>
2019-06-02 09:21:50 +0800

    11221

--- 美术馆/loginForm.cs
+++ 美术馆/loginForm.cs
@@ -33,75 +33,66 @@ namespace 美术馆
             {
                 int flag=0;
                 string ConStr = "Data Source=101.132.124.13;Initial Catalog=美术馆;User ID=sa;Password=123";
-                conn = new SqlConnection(ConStr);                
-                try
-                {
-                    conn.Open();
-                    string sql = "SELECT 职位,工号,密码 from 员工信息表 where 工号='" + userid + "'";
-                    SqlCommand cmd = new SqlCommand(sql, conn);
-                    cmd.CommandType = CommandType.Text;
-                    SqlDataReader sdr;
-                    sdr = cmd.ExecuteReader();
-                    while (sdr.Read())         //从结果中找到
+                conn = new SqlConnection(ConStr);
+                conn.Open();
+                string sql = "SELECT 职位,工号,密码 from 员工信息表 where 工号='" + userid  + "'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+                SqlDataReader sdr;
+                sdr = cmd.ExecuteReader();
+                while (sdr.Read())         //从结果中找到
+                {
+                    if (sdr[1].ToString().Trim() == textBox1.Text.Trim())
                     {
-                        if (sdr[1].ToString().Trim() == textBox1.Text.Trim())
-                        {
-                            flag = 1;
-                            break;
-                        }
+                        flag = 1;
+                        break;
                     }
-                    if (flag == 1)
+                }
+                if (flag == 1)               
+                {                
+                    if (sdr[2].ToString().Replace(" ","") == textBox2.Text.Replace(" ",""))
                     {
-                        if (sdr[2].ToString().Replace(" ", "") == textBox2.Text.Replace(" ", ""))
+                        MessageBox.Show("成功登录");
+                        string position = sdr[0].ToString();
+                        int id = Int32.Parse(textBox1.Text);
+                        if (position.Replace(" ","").Equals("藏品管理员"))
                         {
-                            MessageBox.Show("成功登录");
-                            string position = sdr[0].ToString();
-                            int id = Int32.Parse(textBox1.Text);
-                            if (position.Replace(" ", "").Equals("藏品管理员"))
-                            {
-                                index i = new index(this, id);
-                                this.Hide();
-                                i.Show();
-                            }
-                            else if (position.Replace(" ", "").Equals("专家"))
-                            {
-                                expert_main main = new expert_main(this, id);
-                                this.Hide();
-                                main.Show();
-                            }
-                            else if (position.Replace(" ", "").Equals("票务管理员"))
-                            {
-                                piaowu_main main = new piaowu_main(this, id);
-                                this.Hide();
-                                main.Show();
-                            }
-                            //else if (position.Replace(" ", "").Equals("通知管理员"))
-                            //{
-                            //    expert_main main = new expert_main(this, id);
-                            //    this.Hide();
-                            //    main.Show();
-                            //}
+                            index i = new index(this, id);
+                            this.Hide();
+                            i.Show();
                         }
-                        else
+                        else if (position.Replace(" ", "").Equals("专家"))
                         {
-                            MessageBox.Show("密码错误", "提示");
-                            textBox2.Text = "";
+                            expert_main main = new expert_main(this, id);
+                            this.Hide();
+                            main.Show();
                         }
+                        else if (position.Replace(" ", "").Equals("票务管理员"))
+                        {
+                            piaowu_main main = new piaowu_main(this,id);
+                            this.Hide();
+                            main.Show();
+                        }
+                        //else if (position.Replace(" ", "").Equals("通知管理员"))
+                        //{
+                        //    expert_main main = new expert_main(this, id);
+                        //    this.Hide();
+                        //    main.Show();
+                        //}
                     }
                     else
                     {
-                        MessageBox.Show("用户名不存在", "提示");
-                        textBox1.Text = "";
+                        MessageBox.Show("密码错误", "提示");
                         textBox2.Text = "";
                     }
-                    sdr.Close();
                 }
-                catch (Exception ex)
+                else
                 {
-
-                    MessageBox.Show(ex.Message);
+                    MessageBox.Show("用户名不存在", "提示");
+                    textBox1.Text = "";
+                    textBox2.Text = "";
                 }
-               
+                sdr.Close();
             }
         }
 
